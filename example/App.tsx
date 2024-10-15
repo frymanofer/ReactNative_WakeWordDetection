31a98813f729a42908ccd1b15445a3eb9a300bed
Ofer Fryman <ofer@getecho.io>
2024-10-15 11:46:34 +0300

    .

--- example/App.tsx
+++ example/App.tsx
@@ -216,7 +216,8 @@ type DetectionCallback = (event: any) => void;
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const [isFlashing, setIsFlashing] = useState(false);
-  const wakeWordFile = "need_help_now.onnx";
+  //const wakeWordFile = "need_help_now.onnx";
+  const wakeWordFile = "hey_pango.onnx";
   const wakeWord = formatWakeWord(wakeWordFile);
 
   const backgroundStyle = {
